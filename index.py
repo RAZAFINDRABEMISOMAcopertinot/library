82d2169f117fa1fbcf6561af2d0f183b5972d6bf
Coperty GUI <rhoom.shardtinot@gmail.com>
2022-10-20 08:58:47 +0200

    First commit

---
+++ index.py
@@ -0,0 +1,37 @@
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+import sys
+import pymysql
+
+from PyQt5.uic import loadUiType
+
+ui, _ = loadUiType('library.ui')
+
+class MainApp(QMainWindow, ui):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.Handle_UI_Changes()
+
+    def Handle_UI_Changes(self):
+        self.Hide_Theme()
+
+    def Handle_Buttons(self):
+        pass
+
+    def Show_Theme(self):
+        self.groupBox_6.show()
+
+    def Hide_Theme(self):
+        self.groupBox_6.hide()
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
