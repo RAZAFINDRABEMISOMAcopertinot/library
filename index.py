8e640d88592a661014bfe1e24090d35306c2d2fa
Coperty GUI <rhoom.shardtinot@gmail.com>
2022-10-20 10:24:17 +0200

    Link buttonPush and TabWidget

--- index.py
+++ index.py
@@ -17,16 +17,39 @@ class MainApp(QMainWindow, ui):
 
     def Handle_UI_Changes(self):
         self.Hide_Theme()
+        # self.tabWidget.tabBar().setVisible(False)
 
     def Handle_Buttons(self):
         self.pushButton_5.clicked.connect(self.Show_Theme)
         self.pushButton_21.clicked.connect(self.Hide_Theme)
+
+        self.pushButton.clicked.connect(self.Open_Day_To_Day_Tab)
+        self.pushButton_2.clicked.connect(self.Open_Books_Tab)
+        self.pushButton_3.clicked.connect(self.Open_Users_Tab)
+        self.pushButton_4.clicked.connect(self.Open_Settings_Tab)
 
     def Show_Theme(self):
         self.groupBox_6.show()
 
     def Hide_Theme(self):
         self.groupBox_6.hide()
+
+    #######################################################
+    ############## Opening tabs ###########################
+
+    def Open_Day_To_Day_Tab(self):
+        self.tabWidget.setCurrentIndex(0)
+
+
+    def Open_Books_Tab(self):
+        self.tabWidget.setCurrentIndex(1)
+
+
+    def Open_Users_Tab(self):
+        self.tabWidget.setCurrentIndex(2)
+
+    def Open_Settings_Tab(self):
+        self.tabWidget.setCurrentIndex(3)
 
 
 def main():
